f3579647819822c75d273ad6ede7a4c2cf4cc682
Sandesh Dighe <sandeshdighe95@gmail.com>
2024-03-04 22:26:59 +0530

    Random_color Genrater

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Accordian from './Components/accordian';
 import './App.css'
+import RandomColor from './Components/randomcolor';
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      <Accordian />
+      {/* <Accordian /> */}
+
+      {/* RandomColor */}
+      <RandomColor />
      
     </div>
   );
